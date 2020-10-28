41c66f3862aaca94995366b39e35199163bfc1e3
Brennan Predmore <brennnann@gmail.com>
2020-10-28 06:49:06 -0500

    props init for ApprovalCard / CommentDetail using children as well

--- src/index.js
+++ src/index.js
@@ -2,14 +2,41 @@ import React from 'react';
 import ReactDom from 'react-dom';
 import faker from 'faker';
 import CommentDetail from './CommentDetail';
+import ApprovalCard from './ApprovalCard';
 
 const App = () => {
   return (
     <div className='ui container comments'>
-      <CommentDetail />
-      <CommentDetail />
-      <CommentDetail />
-      <CommentDetail />
+      <ApprovalCard>
+          <div>
+              <h4> Warning!</h4>
+              Are you sure you want to do this?
+          </div>
+      </ApprovalCard>
+      <ApprovalCard >
+        <CommentDetail
+          author='Sam'
+          timeAgo='12:20'
+          comment='I Love to code. I am obsessed with programming'
+          avatar={faker.image.avatar()}
+        />
+             </ApprovalCard>
+             <ApprovalCard>
+        <CommentDetail
+          author='Alex'
+          timeAgo='01:12'
+          comment="It's the best. Its all I do"
+          avatar={faker.image.avatar()}
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author='Jane'
+          timeAgo='16:43'
+          comment='So is Brennan. Hes the best'
+          avatar={faker.image.avatar()}
+        />
+      </ApprovalCard>
     </div>
   );
 };
